9971ef2a4b6a58e8cc50c7ba8ab7a75823357334
Sebastian Lindberg <seb.lindberg@gmail.com>
2016-08-08 01:06:23 +0200

    Began stubbing out the functionallity of Sequence

--- lib/timed/sequence.rb
+++ lib/timed/sequence.rb
@@ -1,5 +1,29 @@
 module Timed
   class Sequence
     include Linked::List
+    
+    # Returns the length of the enture sequence
+    
+    def length
+      
+    end
+    
+    # Returns the total time made up by the items
+    
+    def time
+      
+    end
+    
+    # Returns a new sequence with items that make up the intersection between the two sequences.
+    
+    def intersection(other)
+      
+    end
+    
+    # More efficient than first calling #intersection and the #time on the result.
+    
+    def intersection_time(other)
+      
+    end
   end
 end
